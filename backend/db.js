e35bc8b5061726f81d237cafa932b016ddb8b7f3
Amen Hounkanrin <hounkanrinamen2@gmail.com>
2026-06-06 19:51:17 +0100

    Fix MySQL pool connection

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,23 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('Erreur connexion DB:', err);
     return;
   }
-  console.log('Connecté à MariaDB !');
+  console.log('Connecté à MySQL !');
+  connection.release();
 });
 
-module.exports = db;
+module.exports = pool;
